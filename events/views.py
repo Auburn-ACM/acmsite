27b13596c21698707ac50227da676ba10255ce5f
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-12-17 19:09:51 -0600

    added events page

--- events/views.py
+++ events/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from datetime import datetime
 from .models import Event
 
 # Create your views here.
 
 def index(request):
     event_list = Event.objects.order_by('date')
+    curr_date = datetime.now().date()
+    upcoming_event_list = []
+    past_event_list = []
+    for event in event_list:
+        if event.date >= curr_date:
+            upcoming_event_list.append(event)
+        else:
+            past_event_list.append(event)
+    
     context = {
-        'event_list': event_list,
+        'upcoming_event_list': upcoming_event_list,
+        'past_event_list': past_event_list,
     }
     return render(request, 'events/index.html', context)
 
