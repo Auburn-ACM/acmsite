02abc3ab10fdb7a5f2d96b14848903bc64fbb0c4
Matthew Freestone <matthewfreestone1593@gmail.com>
2023-01-16 17:26:49 -0600

    clubs initial pass

--- acmsite/views.py
+++ acmsite/views.py
@@ -18,3 +18,69 @@ def index(request):
     }
     return render(request, 'acmsite/index.html', context)
 
+def CPT(request):
+    context = {
+        'title': 'CPT',
+        'name': 'Competitive Programming Team',
+        'when': 'TBD',
+        'where': 'TBD',
+        'what': 'TBD',
+        'links': {
+            # 'website': '/clubs/CPT',
+            'discord': 'https://discord.gg/',
+            'groupme': 'https://groupme.com/join_group/',
+            # 'github': 'https://github.com',
+        },
+        'officer_list': (
+            ('President', 'First', 'Last'),
+            # ('Vice President', 'First', 'Last'),
+            # ('Treasurer', 'First', 'Last'),
+            # ('Secretary', 'First', 'Last')
+        )
+    }
+    return render(request, 'acmsite/clubs_base.html', context)
+
+def EHC(request):
+    context = {
+        'title': 'EHC',
+        'name': 'Ethical Hacking Club',
+        'when': 'TBD',
+        'where': 'TBD',
+        'what': 'TBD',
+        'links': {
+            'website': 'https://ehc.auburn.edu',
+            'discord': 'https://discord.gg/',
+            'groupme': 'https://groupme.com/join_group/',
+            'github': 'https://github.com',
+        },
+        'officer_list': (
+            ('President', 'First', 'Last'),
+            ('Vice President', 'First', 'Last'),
+            ('Treasurer', 'First', 'Last'),
+            ('Secretary', 'First', 'Last')
+        )
+    }
+    return render(request, 'acmsite/clubs_base.html', context)
+
+def makerspace(request):
+    context = {
+        'title': 'CS Makerspace',
+        'name': 'CS Makerspace',
+        'when': 'TBD',
+        'where': 'TBD',
+        'what': 'TBD',
+        'links': {
+            # 'website': 'https://ehc.auburn.edu',
+            'discord': 'https://discord.gg/',
+            'groupme': 'https://groupme.com/join_group/',
+            # 'github': 'https://github.com',
+        },
+        'officer_list': (
+            ('President', 'First', 'Last'),
+            ('Vice President', 'First', 'Last'),
+            ('Treasurer', 'First', 'Last'),
+            ('Secretary', 'First', 'Last')
+        )
+    }
+    return render(request, 'acmsite/clubs_base.html', context)
+
